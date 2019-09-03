8d35a7d06dd4f8123e4db1e1051a2eff62671b42
Ilya Sidorchik <ilya@sidorchik.ru>
2019-09-04 01:28:01 +0300

    Extend login form validation

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -39,10 +39,10 @@ const BasicFormSchema = Yup.object().shape({
     username: Yup.string()
         .required("Необходимо заполнить поле")
         .email("Неверная электронная почта")
-        .matches(/(test@test.com)/, "Неверная электронная почта"),
+        .matches(/^(test@test.com)$/, "Неверная электронная почта"),
     password: Yup.string()
         .required("Необходимо заполнить поле")
-        .matches(/(123123)/, "Неверный пароль")
+        .matches(/^(123123)$/, "Неверный пароль")
 });
 
 const UppercasingTextField = (props) => (
