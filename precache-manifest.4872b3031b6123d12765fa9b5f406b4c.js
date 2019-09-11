c6a9ff092b50f83f6f366a2bb4e09ef2fc759071
Ilya Sidorchik <ilya@sidorchik.ru>
2019-09-11 18:41:20 +0300

    Updates

---
+++ precache-manifest.4872b3031b6123d12765fa9b5f406b4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2e8097f48cb536850f310381902eec3",
+    "url": "/loftschool-react-final-project/index.html"
+  },
+  {
+    "revision": "037d29a7b25dd33ae930",
+    "url": "/loftschool-react-final-project/static/css/main.63a4ef81.chunk.css"
+  },
+  {
+    "revision": "ae703611d18d6fd6b61b",
+    "url": "/loftschool-react-final-project/static/js/2.154bc12d.chunk.js"
+  },
+  {
+    "revision": "037d29a7b25dd33ae930",
+    "url": "/loftschool-react-final-project/static/js/main.c2038d05.chunk.js"
+  },
+  {
+    "revision": "e0692c9ada9338e5924c",
+    "url": "/loftschool-react-final-project/static/js/runtime~main.a20ee360.js"
+  }
+]);
