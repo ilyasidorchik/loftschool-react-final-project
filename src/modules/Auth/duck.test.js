315134fceb9d958b8003e567f13d2a3a17144975
Ilya Sidorchik <ilya@sidorchik.ru>
2019-09-15 18:43:23 +0300

    Test Auth reducers

--- src/modules/Auth/duck.test.js
+++ src/modules/Auth/duck.test.js
@@ -1,49 +1,106 @@
+import * as constants from "./constants";
 import reducer, {
 	fetchAuthRequest,
 	fetchAuthSuccess,
 	fetchAuthFailure,
 	fetchLogoutRequest
 } from "./duck";
-import * as constants from "./constants";
 
-describe("actions", () => {
-	it("should create an action to fetch auth request", () => {
-		const expectedAction = {
-			type: constants.FETCH_AUTH_REQUEST,
-			payload: {
-				userName: "ivanivanov",
-				userPassword: "qwerty1234"
-			}
-		};
-		expect(
-			fetchAuthRequest({ userName: "ivanivanov", userPassword: "qwerty1234" })
-		).toEqual(expectedAction);
+describe("Auth Duck", () => {
+	describe("actions", () => {
+		it("should create an action to fetch auth request", () => {
+			const expectedAction = {
+				type: constants.FETCH_AUTH_REQUEST,
+				payload: {
+					userName: "ivanivanov",
+					userPassword: "qwerty1234"
+				}
+			};
+			expect(
+				fetchAuthRequest({ userName: "ivanivanov", userPassword: "qwerty1234" })
+			).toEqual(expectedAction);
+		});
+
+		it("should create an action to fetch auth success", () => {
+			const expectedAction = {
+				type: constants.FETCH_AUTH_SUCCESS
+			};
+			expect(fetchAuthSuccess()).toEqual(expectedAction);
+		});
+
+		it("should create an action to fetch auth failure", () => {
+			const expectedAction = {
+				type: constants.FETCH_AUTH_FAILURE,
+				payload: {
+					success: false,
+					error: "Сообщение об ошибке"
+				}
+			};
+			expect(
+				fetchAuthFailure({ success: false, error: "Сообщение об ошибке" })
+			).toEqual(expectedAction);
+		});
+
+		it("should create an action to fetch logout request", () => {
+			const expectedAction = {
+				type: constants.FETCH_LOGOUT_REQUEST
+			};
+			expect(fetchLogoutRequest()).toEqual(expectedAction);
+		});
 	});
 
-	it("should create an action to fetch auth success", () => {
-		const expectedAction = {
-			type: constants.FETCH_AUTH_SUCCESS
-		};
-		expect(fetchAuthSuccess()).toEqual(expectedAction);
-	});
+	describe("reducers", () => {
+		const state0 = reducer(undefined, {});
 
-	it("should create an action to fetch auth failure", () => {
-		const expectedAction = {
-			type: constants.FETCH_AUTH_FAILURE,
-			payload: {
-				success: false,
-				error: "Сообщение об ошибке"
-			}
-		};
-		expect(
-			fetchAuthFailure({ success: false, error: "Сообщение об ошибке" })
-		).toEqual(expectedAction);
-	});
+		it("should return the initial state", () => {
+			expect(state0).toEqual({
+				isAuthorized: false
+			});
+		});
 
-	it("should create an action to fetch logout request", () => {
-		const expectedAction = {
-			type: constants.FETCH_LOGOUT_REQUEST
-		};
-		expect(fetchLogoutRequest()).toEqual(expectedAction);
+		it("should handle FETCH_AUTH_REQUEST", () => {
+			const state1 = reducer(state0, {
+				type: constants.fetchAuthRequest,
+				payload: {
+					userName: "ivanivanov",
+					userPassword: "qwerty1234"
+				}
+			});
+
+			expect(state1).toEqual({
+				isAuthorized: false
+			});
+		});
+
+		it("should handle FETCH_AUTH_SUCCESS", () => {
+			const state1 = reducer(state0, fetchAuthSuccess());
+
+			expect(state1).toEqual({
+				isAuthorized: true
+			});
+		});
+
+		it("should handle FETCH_AUTH_FAILURE", () => {
+			const state1 = reducer(state0, {
+				type: constants.fetchAuthFailure,
+				payload: {
+					error: "Сообщение об ошибке"
+				}
+			});
+
+			expect(state1).toEqual({
+				isAuthorized: false
+			});
+		});
+
+		it("should handle FETCH_LOGOUT_REQUEST", () => {
+			const state1 = reducer(state0, fetchAuthSuccess());
+
+			const state2 = reducer(state1, fetchLogoutRequest());
+
+			expect(state2).toEqual({
+				isAuthorized: false
+			});
+		});
 	});
 });
